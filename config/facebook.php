9ba7dcc99de5b7135fa341fe7de88db1b429eb76
Darío Rivera <fermius.us@gmail.com>
2020-05-31 17:08:49 -0500

    feat: add ending article like button

--- config/facebook.php
+++ config/facebook.php
@@ -44,19 +44,37 @@ return [
     ],
 
     'posts' => [
-        // true | false
-        'activated' => env('FB_SOCIAL_PLUGINS_POSTS_ACTIVATED', true),
+        'starting' => [
+            // true | false
+            'activated' => env('FB_SOCIAL_PLUGINS_POSTS_STARTING_ACTIVATED', true),
 
-        // standard | box_count | button_count | button
-        'layout' => env('FB_SOCIAL_PLUGINS_LAYOUT_POSTS', 'box_count'),
+            // standard | box_count | button_count | button
+            'layout' => env('FB_SOCIAL_PLUGINS_POSTS_STARTING_LAYOUT', 'box_count'),
 
-        // like | recommend
-        'action' => env('FB_SOCIAL_PLUGINS_ACTION_POSTS', 'like'),
+            // like | recommend
+            'action' => env('FB_SOCIAL_PLUGINS_POSTS_STARTING_ACTION', 'like'),
 
-        // small | large
-        'size' => env('FB_SOCIAL_PLUGINS_SIZE_POSTS', 'small'),
+            // small | large
+            'size' => env('FB_SOCIAL_PLUGINS_POSTS_STARTING_SIZE', 'small'),
 
-        // true | false
-        'share_button' => env('FB_SOCIAL_PLUGINS_SHARE_BUTTON_POSTS', 'true'),
+            // true | false
+            'share_button' => env('FB_SOCIAL_PLUGINS_POSTS_STARTING_SHARE_BUTTON', 'true'),
+        ],
+        'ending' => [
+            // true | false
+            'activated' => env('FB_SOCIAL_PLUGINS_POSTS_ENDING_ACTIVATED', true),
+
+            // standard | box_count | button_count | button
+            'layout' => env('FB_SOCIAL_PLUGINS_POSTS_ENDING_LAYOUT', 'button'),
+
+            // like | recommend
+            'action' => env('FB_SOCIAL_PLUGINS_POSTS_ENDING_ACTION', 'like'),
+
+            // small | large
+            'size' => env('FB_SOCIAL_PLUGINS_POSTS_ENDING_SIZE', 'large'),
+
+            // true | false
+            'share_button' => env('FB_SOCIAL_PLUGINS_POSTS_ENDING_SHARE_BUTTON', 'false'),
+        ],
     ],
 ];
