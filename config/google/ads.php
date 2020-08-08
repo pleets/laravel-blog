15cd0d8ad95be4cb9dbb4ee5da3e9a6595d9123b
Darío Rivera <fermius.us@gmail.com>
2020-08-08 10:55:54 -0500

    refactor: split google configurations

---
+++ config/google/ads.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    // true | false
+    'activated' => env('GOOGLE_ADS_ACTIVATED', false),
+    'client' => env('GOOGLE_ADS_CLIENT'),
+];
---
+++ config/google/analytics.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    // true | false
+    'activated' => env('GOOGLE_ANALYTICS_ACTIVATED', false),
+    'id' => env('GOOGLE_ANALYTICS_ID'),
+];
