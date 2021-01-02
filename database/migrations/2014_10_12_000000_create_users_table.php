380f58dc7cde906574615ebac9a530898eda496f
Darío Rivera <fermius.us@gmail.com>
2021-01-02 08:38:52 -0500

    feat: upgrade to L7

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,7 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('user_id');
+            $table->id('user_id');
             $table->unsignedBigInteger('profile_id')->nullable();
             $table->string('name');
             $table->string('email')->unique();
