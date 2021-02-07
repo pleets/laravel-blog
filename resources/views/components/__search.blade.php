9a394b8807719294af5143ee91f7bf31312b50ea
Darío Rivera <fermius.us@gmail.com>
2021-02-07 11:07:50 -0500

    feat: no autocomplete in search form

--- resources/views/components/__search.blade.php
+++ resources/views/components/__search.blade.php
@@ -2,6 +2,7 @@
     <div class="input-group input-group-sm">
         {{ Form::text('search', Request::get('search'), [
                     'class' => 'form-control',
+                    'autocomplete' => 'off',
                     ]) }}
         <div class="input-group-append">
             {{ Form::submit('Search', ['class' => 'btn btn-primary']) }}
